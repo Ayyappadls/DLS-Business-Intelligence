b5f0a0fe32023ea81eded763a961cfd84a0b10c4
Ayyappadls <166862953+Ayyappadls@users.noreply.github.com>
2026-09-07 10:19:02 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import BusinessMirror from "../BusinessMirror.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BusinessMirror />
+  </React.StrictMode>
+);
